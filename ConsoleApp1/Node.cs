0e2160192158d78c5c5be022e0fa6370f007bac2
munyite-megzs <margaret.sitati1@gmail.com>
2019-02-07 12:54:30 +0300

    finished creating the node class

--- ConsoleApp1/Node.cs
+++ ConsoleApp1/Node.cs
@@ -6,5 +6,23 @@ namespace ConsoleApp1
 {
     class Node
     {
+        private object data;
+        private Node next;
+
+        public Node(object data, Node next)
+        {
+            this.data = data;
+            this.next = next;
+        }
+
+        public object Data {
+            get { return this.data; }
+            set { this.data = value; }
+        }
+        public Node Next {
+            get { return this.next; }
+            set {this.next = value; }
+        }
+
     }
 }

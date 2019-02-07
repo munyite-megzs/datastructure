df7da855e0d86de872c228c823a83f7dc109f144
munyite-megzs <margaret.sitati1@gmail.com>
2019-02-07 15:09:34 +0300

    added the add at begin and end methods

---
+++ ConsoleApp1/LinkedList.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class LinkedList
+    {
+        private Node head;
+        private int count;
+
+        public LinkedList()
+        {
+            this.head = null;
+            this.count = 0; 
+
+        }
+
+        public bool Empty
+        {
+            get { return this.count == 0; }
+        }
+
+        public int Count
+        {
+            get { return this.count; }
+        }
+
+        public Object Add(int index, object o)
+        {
+            if (index < 0){
+                throw new ArgumentOutOfRangeException("");
+            }
+
+            if (index > count)
+            {
+                index = count;
+            }
+
+            Node current = this.head;
+            // this insertion happens before a node at a particular index.
+            if (this.Empty || index==0)
+            {
+                this.head = new Node(o, this.head);
+            }
+            else
+            {
+                for (int i = 0; i < index - 1; i++)
+                {
+                    current = current.Next;
+                }
+
+                current.Next = new Node(o, current.Next);
+            }
+
+            count++;
+
+            return o;
+        } 
+
+        //Add at the end of the list 
+
+        public object Add(object o)
+        {
+            return this.Add(count,o);
+        }
+    }
+}
